5865ac88e3d842d0e63260e0809d573a71a38220
DavidOConnor1 <146002230+DavidOConnor1@users.noreply.github.com>
2024-02-12 11:42:38 +0000

    I wonder if this uploads twice

---
+++ DoubleLinkList/src/doublelinklist/Interface.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package doublelinklist;
+
+/**
+ *
+ * @author dmoc2
+ */
+public interface Interface {
+    
+    public boolean isEmpty();
+    public int size();
+    public void add(int index, Object element);
+    public Object get(int index);
+    public void remove(int index, Object element);
+    public void printList();
+    
+    
+    
+    
+}
