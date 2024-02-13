7ec8d3609345444e0770a68ef146247780ca41a9
DavidOConnor1 <146002230+DavidOConnor1@users.noreply.github.com>
2024-02-13 10:21:23 +0000

    maths stiff

---
+++ factorial/factorial/src/factorial.java
@@ -0,0 +1,15 @@
+public class factorial {
+    public static void main(String[] args) {
+        int fact2 = factoral(5);
+
+        System.out.println(fact2);
+    }
+
+    public static int factoral(int n){
+        if(n == 0){
+            return 1;
+        } else {
+            return n *factoral(n-1);
+        }
+    }
+}
