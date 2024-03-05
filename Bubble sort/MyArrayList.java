b12e5769173a8d51d022e1345beab9fe70f5379f
DavidOConnor1 <146002230+DavidOConnor1@users.noreply.github.com>
2024-03-05 10:40:38 +0000

    I have finished the better sort bubble

--- Bubble sort/MyArrayList.java
+++ Bubble sort/MyArrayList.java
@@ -1,3 +1,39 @@
-public class MyArrayList {
+/*
+ * This is blue print class 
+ * Author David O'Connor 
+ */
+
+import java.util.ArrayList;
+
+import javax.lang.model.element.Element;
+
+public class MyArrayList<ElemType> extends ArrayList<ElemType>{
+    
+    public void betterBubble(){
+        Comparable elemAtJ; //using the comparable interface
+        Comparable elmemAtJplus;
+
+        for(int i=0; i < size(); i++){
+            for(int j=0; j < size() -1-i; j++){
+                elemAtJ = (Comparable)get(j); //getting the items at the required index
+                elmemAtJplus = (Comparable)get(j+1); //casting the variables to compare them
+
+                if (elemAtJ.compareTo(elmemAtJplus)> 0) {
+                    swap(j, j+1);
+                }//end if 
+            } //end for loop 2
+        }//end for loop 1
+    }//end betterBubble
+
+    private void swap(int pos1, int pos2){
+        //temp holder for elements
+        ElemType tempPos1 = get(pos1);
+        ElemType tempPos2 = get(pos2);
+
+        
+        set(pos1, tempPos2);
+        set(pos2, tempPos1);
+         
+    }
     
 }
--- Bubble sort/TestApp.java
+++ Bubble sort/TestApp.java
@@ -1,6 +1,25 @@
+import java.util.Arrays;
+
 public class TestApp {
 
     public static void main(String[] args) {
         
+        MyArrayList<Integer> list1 = new MyArrayList<Integer>();
+        list1.add(7);
+        list1.add(90);
+        list1.add(2);
+        list1.add(4);
+        list1.add(33);
+        list1.add(58);
+
+        System.out.println(list1);
+
+        list1.betterBubble();
+        System.out.println("After sort: "+list1);
+        
+        
+        //add unsorted items to your arraylist, print list 1
+        //sort using the better bubble method and then print list 1 again to ensure it is 
+
     }
 }
